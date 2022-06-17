63ba0bc99b83acf652dc6d6e96424c55d9bfb257
Frank Hui <huifrank@live.com>
2022-06-17 01:30:34 -0700

    Overhaul with event role management

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-from __future__ import print_function
-
 import datetime
 import os.path
+import random
 import time
 
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,49 +11,13 @@ from os import path
 import discord
 import json
 
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 data = None
-# SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-event_roles_map = {}
 
 def save():
     with open('database.json', 'w') as outfile:
         json.dump(data, outfile)
-
-def modify(*args):
-    curr = data[args[0]]
-    for i in range(1,len(args) - 2):
-        curr = curr[args[i]]
-    curr[data[args[-2]]] = data[args[-1]]
-    save()
-
-
-
-
-
-creds = None
-# The file token.json stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-# if os.path.exists('token.json'):
-#     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-# If there are no (valid) credentials available, let the user log in.
-# if not creds or not creds.valid:
-#     if creds and creds.expired and creds.refresh_token:
-#         creds.refresh(Request())
-#     else:
-#         flow = InstalledAppFlow.from_client_secrets_file(
-#             'credentials.json', SCOPES)
-#         creds = flow.run_local_server(port=0)
-#     Save the credentials for the next run
-    # with open('token.json', 'w') as token:
-    #     token.write(creds.to_json())
-#
-# try:
-#     service = build('calendar', 'v3', credentials=creds)
-# except HttpError as error:
-#     print('An error occurred: %s' % error)
 
 @client.event
 async def on_ready():
@@ -72,70 +30,59 @@ async def on_ready():
         data = {}
         save()
 
+    if "event_roles_map" not in data.keys():
+        data["event_roles_map"] = {}
+    print(data["event_roles_map"])
     for guild in client.guilds:
-        for event in guild.fetch_scheduled_events():
+        await guild.fetch_roles()
+        for event in await guild.fetch_scheduled_events():
+            if str(event.id) not in data["event_roles_map"].keys() or not guild.get_role(data["event_roles_map"][str(event.id)]):
+                await on_scheduled_event_create(event)
+            elif event.name != guild.get_role(data["event_roles_map"][str(event.id)]).name + " [TPT]":
+                await guild.get_role(data["event_roles_map"][str(event.id)]).edit(name=event.name + " [TPT]")
+        for role in guild.roles:
+            if role.name.endswith("[TPT]") and role.id not in data["event_roles_map"].values():
+                pass
+                # REMOVE ROLE NOT YET IMPLEMENTED
+                # await event.guild.remove_role(role.id)
 
 
 
 @client.event
-async def on_scheduled_event_create(event):
-    role = await event.guild.create_role(name=event.name, mentionable=True)
-    m = await event.guild.fetch_member(event.creator.id)
-    await m.add_roles(role)
+async def on_scheduled_event_create(event: discord.ScheduledEvent):
+    role = await event.guild.create_role(name=event.name + " [TPT]", mentionable=True, color=discord.Color(0).random())
+    data["event_roles_map"][str(event.id)] = role.id
+    async for user in event.users():
+        m = await event.guild.fetch_member(user.id)
+        await m.add_roles(role)
+    save()
+
+@client.event
+async def on_scheduled_event_delete(event: discord.ScheduledEvent):
+    # REMOVE ROLE NOT YET IMPLEMENTED
+    # await event.guild.remove_role(data["event_roles_map"][str(event.id)])
+    del data["event_roles_map"][str(event.id)]
+
+@client.event
+async def on_scheduled_event_user_add(event: discord.ScheduledEvent, user: discord.User):
+    m = await event.guild.fetch_member(user.id)
+    await m.add_roles(event.guild.get_role(data["event_roles_map"][str(event.id)]))
 
 
 @client.event
-async def on_scheduled_event_user_add(event, user):
-    print("user added to event")
+async def on_scheduled_event_user_remove(event: discord.ScheduledEvent, user: discord.User):
+    m = await event.guild.fetch_member(user.id)
+    await m.remove_roles(event.guild.get_role(data["event_roles_map"][str(event.id)]))
+
+
+
 
 @client.event
 async def on_message(message: discord.Message):
     if message.author == client.user:
         return
-
-    # Integration with Google Calendar
-    # if str(message.guild.id) in list(data.keys()) and message.channel.id == data[str(message.guild.id)]["channel"]:
-    #     if  message.content.endswith("@gmail.com"):
-    #         try:
-    #             rule = {
-    #                 'scope': {
-    #                     'type': 'user',
-    #                     'value': message.content,
-    #                 },
-    #                 'role': 'writer'
-    #             }
-    #
-    #             created_rule = service.acl().insert(calendarId=data[str(message.guild.id)]["cal"], body=rule).execute()
-    #             confirmation = await message.channel.send("Shared to email.")
-    #             time.sleep(3)
-    #             await confirmation.delete()
-    #             await message.delete()
-    #         except HttpError as error:
-    #             print('An error occurred: %s' % error)
-    #             errormsg = await message.channel.send("Something went wrong adding your gmail. Only send your gmail into this channel.")
-    #             time.sleep(5)
-    #             await errormsg.delete()
-    #             await message.delete()
-    #
-    #     else:
-    #         await message.delete()
-    # elif message.content.startswith('-'):
-    #     command = message.content[1:].split(" ")[0]
-    #     param = message.content[1:].split(" ")[1:]
-    #     if command == "register":
-    #         if len(param) != 1:
-    #             await message.channel.send("Usage: `-register CALENDARID`")
-    #             return
-    #         data[str(message.guild.id)] = {'channel':message.channel.id, 'cal':param[0]}
-    #         save()
-    #         confirmation = await message.channel.send("Channel registered for email subscription.")
-    #         time.sleep(3)
-    #         await confirmation.delete()
-    #         await message.delete()
-    #         calendar = service.calendars().get(calendarId=param[0]).execute()
-    #         await message.channel.send("Send your gmail address into this channel to have " + calendar['summary'] + " shared with you. All messages posted to this channel will be deleted.\nExample: gonzalo@gmail.com")
-
-
+    if client.user in message.mentions:
+        await message.channel.send("bonk")
 
 client.run(os.getenv("DISCORD"))
 
