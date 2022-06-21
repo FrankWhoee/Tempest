3e83fe308a8fefea04d43be11afef261d9df5eba
FrankWhoee <huifrank@live.com>
2022-06-20 20:01:07 -0700

    Add colour accessibility to roles

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import datetime
 import os.path
-import random
+from random import random
 import time
+import wcag_contrast_ratio as contrast
 
 from dotenv import load_dotenv
 
@@ -14,6 +15,10 @@ import json
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 data = None
+
+# CONSTANTS
+dark_mode_background = (54/255,57/255,63/255)
+white_mode_background = (1,1,1)
 
 def save():
     with open('database.json', 'w') as outfile:
@@ -69,11 +74,25 @@ async def on_ready():
     save()
     print("Finished cleaning up and catching up to events.")
 
+def generate_accessible_colour():
+    role_colour = (random(), random(), random())
+    contrast_dark = contrast.rgb(role_colour, dark_mode_background)
 
+    # Generate a random colour until it is a good contrast
+    attempt = 0
+    while contrast_dark < 4.5 and attempt < 25:
+        role_colour = (random(), random(), random())
+        contrast_dark = contrast.rgb(role_colour, dark_mode_background)
+        attempt += 1
+    if attempt == 25:
+        print("Could not generate a good contrast colour")
+    role_colour = (int(role_colour[0] * 255), int(role_colour[1] * 255), int(role_colour[2] * 255))
+    role_colour = discord.Color.from_rgb(role_colour[0], role_colour[1], role_colour[2])
+    return role_colour
 
 @client.event
 async def on_scheduled_event_create(event: discord.ScheduledEvent):
-    role = await event.guild.create_role(name=event.name + " [TPT]", mentionable=True, color=discord.Color(0).random())
+    role = await event.guild.create_role(name=event.name + " [TPT]", mentionable=True, color=generate_accessible_colour())
     data["event_role_map"][str(event.id)] = role.id
     async for user in event.users():
         m = await event.guild.fetch_member(user.id)
@@ -88,8 +107,11 @@ async def on_scheduled_event_delete(event: discord.ScheduledEvent):
 
 @client.event
 async def on_scheduled_event_user_add(event: discord.ScheduledEvent, user: discord.User):
-    m = await event.guild.fetch_member(user.id)
-    await m.add_roles(event.guild.get_role(data["event_role_map"][str(event.id)]))
+    try:
+        m = await event.guild.fetch_member(user.id)
+        await m.add_roles(event.guild.get_role(data["event_role_map"][str(event.id)]))
+    except:
+        print("Could not add user to event.")
 
 
 @client.event
